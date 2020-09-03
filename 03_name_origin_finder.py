cc65060b811a12bfeced73d0aef0d89f56850658
Mathu Deivakumar <madhu619@gmail.com>
2020-09-03 22:30:44 +0530

    Update 03_name_origin_finder.py

--- 03_name_origin_finder.py
+++ 03_name_origin_finder.py
@@ -4,7 +4,7 @@ from fuzzywuzzy import process
 import pandas as pd
 
 # Import test data to be processed
-data_raw = pd.read_csv('C:/Users/Mathu/Desktop/GNS Automation/sample.csv')
+data_raw = pd.read_csv('sample.csv')
 
 # Function to find the origin of the name supplied by the df
 def name_orgin_finder(name):
@@ -14,25 +14,12 @@ def name_orgin_finder(name):
 origin = []
 
 # Loop to find the origin of each name and append it to the initiated list file
-for key, value in data_raw['FIELD_6'].iteritems():
-    origin.append(name_orgin_finder(data_raw.loc[key, 'FIELD_6']))
+for key, value in data_raw['column_c'].iteritems():
+    origin.append(name_orgin_finder(data_raw.loc[key, 'column_c']))
 print(origin)    
 
 # Merge the results to the original df
 data_raw['ORIGIN'] = origin
 
 # Export the table to excel file
-data_raw.to_excel('C:/Users/Mathu/Desktop/GNS Automation/name_origins.xlsx', index=False)
-
-#################################
-# Test
-process.extract(data_raw.iloc[0,2], names_cleaned_dict_final)
-process.extractOne('Nguymn thu li chin', names_cleaned_dict_final)
-process.extract('Nguyen thu li chin', names_cleaned_dict_final)
-process.extract('Nguyen', names_cleaned_dict_final)
-process.extract('thu', names_cleaned_dict_final)
-process.extract('li', names_cleaned_dict_final)
-process.extract('chin', names_cleaned_dict_final)
-
-
-
+data_raw.to_excel('name_origins.xlsx', index=False)
